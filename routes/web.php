2e747290a41babd0304deba5a9f76adf42524086
nadli3417 <nadli3417@gmail.com>
2024-10-03 03:24:31 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/users', function () {
+    foreach(User::all() as $user) {
+        dump($user-> name);
+    }
+});
